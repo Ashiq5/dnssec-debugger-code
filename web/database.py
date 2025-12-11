8112eb7065799305d8af679ff452529c2bf38c88
ashiq5 <ishtiaqashiq5@gmail.com>
2025-12-11 13:20:10 -0500

    minor

--- web/database.py
+++ web/database.py
@@ -5,7 +5,13 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URI")
+PROTOCOL = os.getenv("PROTOCOL")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASS")
+DB_HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+DB_NAME = os.getenv("DB_NAME")
+SQLALCHEMY_DATABASE_URL = PROTOCOL + "://" + DB_USER + ":" + DB_PASSWORD + "@" + DB_HOST + ":" + PORT + "/" + DB_NAME
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
